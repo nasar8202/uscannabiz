41f5ebdf5ea82a9436a6d7da3f8f87b115897115
nasar8202 <105274484+nasar8202@users.noreply.github.com>
2022-07-22 12:35:44 +0500

    send mail to customer order detail

--- app/Http/Controllers/Vendor/VendorRequestController.php
+++ app/Http/Controllers/Vendor/VendorRequestController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\VendorRequest;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
-
+use App\Models\Product;
 class VendorRequestController extends Controller
 {
     /**
@@ -55,13 +55,15 @@ class VendorRequestController extends Controller
             //     'password' => Hash::make($request->input('first_name')),
             //     'role_id' => 4,
             // ]);
+
+
         $vendor = new VendorRequest;
         $vendor->product_id = $request->input('product_id');
-        $vendor->vendor_id = $request->input('vendor_id'); 
-        $vendor->full_name = $request->input('full_name'); 
-        $vendor->phone_num = $request->input('phone_num'); 
-        $vendor->email = $request->input('email'); 
-        $vendor->address = $request->input('address'); 
+        $vendor->vendor_id = $request->input('vendor_id');
+        $vendor->full_name = $request->input('full_name');
+        $vendor->phone_num = $request->input('phone_num');
+        $vendor->email = $request->input('email');
+        $vendor->address = $request->input('address');
         $vendor->city = $request->input('city');
         $vendor->quantity = $request->input('quantity');
         $auth = Auth::user();
@@ -69,8 +71,20 @@ class VendorRequestController extends Controller
             $vendor->customer_id = $auth->id;
         }
         $vendor->save();
+        $product = Product::where('id',$request->input('product_id'))->first();
 
-        
+        $details = [
+            'name'=> $request->full_name,
+            'email' => $request->input('email'),
+            'phone_num' => $request->input('phone_num'),
+            'city' => $request->input('city'),
+            'quantity' => $request->input('quantity'),
+            'address'=>  $request->input('address'),
+            'product_name'=>$product->product_name,
+            'sku'=>$product->sku
+        ];
+
+        \Mail::to($request->input('email'))->send(new \App\Mail\SendEmailAdminCustomerBroker($details));
         return redirect()->route('order_thanks')->with('success',"Request Has Been Submited");
         }
     }
